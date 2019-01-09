75cc83a36cc7e0648c211a1fef35c3516a65f41a
Charlie Llewellin <charlie@llewellin.net>
2019-01-09 08:55:25 -0500

    set up javascript

---
+++ js/etype_paywall.js
@@ -0,0 +1,34 @@
+/* add custom js */
+(function ($) {
+    'use strict';
+    Drupal.behaviors.etype_paywall = {
+        attach: function (context, settings) {
+
+            var setCookie = function(name,value,days) {
+                var expires = "";
+                if (days) {
+                    var date = new Date();
+                    date.setTime(date.getTime() + (days*24*60*60*1000));
+                    expires = "; expires=" + date.toUTCString();
+                }
+                document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+            };
+            var getCookie = function(name) {
+                var nameEQ = name + "=";
+                var ca = document.cookie.split(';');
+                for(var i=0;i < ca.length;i++) {
+                    var c = ca[i];
+                    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+                }
+                return null;
+            };
+            var eraseCookie = function(name) {
+                document.cookie = name+"=; Max-Age=-99999999;";
+            };
+
+            setCookie('ppkcookie','testcookie',7);
+
+        }
+    };
+})(jQuery);
