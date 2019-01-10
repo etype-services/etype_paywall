bbb170320c91c317a1f9c69c9c8381be3681b363
Charlie Llewellin <charlie@llewellin.net>
2019-01-10 12:36:08 -0500

    admin form

--- js/etype_paywall.js
+++ js/etype_paywall.js
@@ -57,6 +57,7 @@
                             height: '100%'
                         });
                         $("#block-system-main").css("display", "none");
+                        $("#block-etype-paywall-etype-paywall-info").css("display", "none");
                         value = Drupal.settings.etype_paywall.limit;
                     }
                     paywall_replace(value);
